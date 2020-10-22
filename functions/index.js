892c40f4d03e00f45bba96915d3274c634ddd589
Scott Daily <scottjuggles@gmail.com>
2020-10-22 08:53:19 -0400

    added initial firebase function routes

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,50 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+admin.initializeApp();
 
- exports.helloWorld = functions.https.onRequest((request, response) => {
-    functions.logger.info("Hello logs!", {structuredData: true});
-    response.send("Hello Time Tracker!");
- });
+const express = require('express');
+const app = express();
+
+app.get('/users', (req, res) => {
+   admin
+   .firestore()
+   .collection('users')
+   .orderBy('createdAt', 'desc')
+   .get()
+   .then((querySnapshot) => {
+      let users = [];
+      querySnapshot.forEach((doc) => {
+         users.push({
+            userId: doc.id,
+            jobs: doc.data().jobs,
+            uid: doc.data().uid,
+            createdAt: doc.data().createdAt
+         });
+      });
+      return res.json(users);
+   })
+   .catch((err) => console.error(err));
+})
+
+app.post('/users', (req, res) => {
+   const newUser = {
+      jobs: req.body.jobs,
+      uid: req.body.uid,
+      createdAt: new Date().toISOString()
+   };
+
+   admin
+      .firestore()
+      .collection('users')
+      .add(newUser)
+      .then(doc => {
+         res.json({ message: `document ${doc.id} created successfully`})
+      })
+      .catch(err => {
+         res.status(500).json({ error: err});
+         console.log(err);
+      });
+});
+
+exports.api = functions.https.onRequest(app);
