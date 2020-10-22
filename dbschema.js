9c067d851a998b749b4d751b7c1fb7aef4dc5555
Scott Daily <scottjuggles@gmail.com>
2020-10-22 13:10:13 -0400

    added ID Validation middleware function

---
+++ dbschema.js
@@ -0,0 +1,10 @@
+let db = {
+    users: [
+        {
+            uid: 'user id',
+            jobs: 'jobs',
+            createdAt: '2019-03-15T11:46:01.018Z',
+            
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -3,8 +3,32 @@ const admin = require('firebase-admin');
 
 admin.initializeApp();
 
+var cors = require('cors');
+
 const express = require('express');
 const app = express();
+app.use(cors());
+
+const validateIdToken = (req, res, next) => {
+   let idToken;
+
+   if (req.headers.authorization && req.headers.authorization.startsWith('Bearer ')) {
+      idToken = req.headers.authorization.split('Bearer ')[1];
+   } else {
+      console.error('No token found');
+      return res.status(403).json({error: 'Invalid access token'});
+   }
+
+   admin.auth().verifyIdToken(idToken)
+      .then(decodedToken => {
+         let uid = decodedToken.uid;
+         console.log('ID Token decoded', decodedToken);
+         return next();
+      }).catch(function(error) {
+         console.log(error);
+         res.status(403).send('Unauthorized');
+      })
+};
 
 app.get('/users', (req, res) => {
    admin
