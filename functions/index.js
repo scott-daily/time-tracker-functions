ac2a8b3a752777079823de5fa7917b7bc074fb5d
Scott Daily <scottjuggles@gmail.com>
2020-10-28 12:01:01 -0400

    added job editing route / functionality

--- functions/index.js
+++ functions/index.js
@@ -6,8 +6,11 @@ admin.initializeApp();
 var cors = require('cors');
 
 const express = require('express');
+const { response } = require('express');
 const app = express();
 app.use(cors());
+
+const db = admin.firestore();
 
 const validateIdToken = (req, res, next) => {
    let idToken;
@@ -45,53 +48,57 @@ exports.newUser = functions.auth.user().onCreate((user) => {
 // Get all users 
 
 app.get('/users', validateIdToken, (req, res) => {
-   console.log('ran users');
-   admin
-   .firestore()
-   .collection('users')
-   .orderBy('createdAt', 'desc')
-   .get()
-   .then((querySnapshot) => {
-      let users = [];
-      querySnapshot.forEach((doc) => {
-         users.push({
-            userId: doc.id,
-            jobs: doc.data().jobs,
-            uid: doc.data().uid,
-            createdAt: doc.data().createdAt
+   db.collection('users')
+      .orderBy('createdAt', 'desc')
+      .get()
+      .then((querySnapshot) => {
+         let users = [];
+         querySnapshot.forEach((doc) => {
+            users.push({
+               userId: doc.id,
+               jobs: doc.data().jobs,
+               uid: doc.data().uid,
+               createdAt: doc.data().createdAt
+            });
          });
-      });
-      return res.json(users);
-   })
-   .catch((err) => console.error(err));
+         return res.json(users);
+      })
+      .catch((err) => console.error(err));
 })
 
 // Get all jobs from a specific user
 
 app.get('/jobs', validateIdToken, (req, res) => {
-   admin
-   .firestore()
-   .collection(`users/${req.body.uid}/jobs`)
-   .orderBy('createdAt', 'desc')
-   .get()
-   .then((querySnapshot) => {
-      let jobs = [];
-      querySnapshot.forEach((doc) => {
-         jobs.push({
-            uid: doc.data().uid,
-            title: doc.data().title,
-            rate: doc.data().rate,
-            createdAt: doc.data().createdAt
+   db.collection(`users/${req.body.uid}/jobs`)
+      .orderBy('createdAt', 'desc')
+      .get()
+      .then((querySnapshot) => {
+         let jobs = [];
+         querySnapshot.forEach((doc) => {
+            jobs.push({
+               uid: doc.data().uid,
+               title: doc.data().title,
+               rate: doc.data().rate,
+               createdAt: doc.data().createdAt
+            });
          });
-      });
-      return res.json(jobs);
-   })
-   .catch((err) => console.error(err));
+         return res.json(jobs);
+      })
+      .catch((err) => console.error(err));
 })
 
-// Add a job to the jobs collection for a specific user
+// Add a job to the jobs collection for a specific user.  
+// TODO: prevent user from creating a job with duplicate title
 
 app.post('/jobs', validateIdToken, (req, res) => {
+   if (req.body.title.trim() === '') {
+      return res.status(400).json({ title: 'Must not be empty'});
+   }
+
+   if (!req.body.rate) {
+      return res.status(400).json({ rate: 'Must not be empty'});
+   }
+
    const jobMap = {
       uid: req.body.uid,
       title: req.body.title,
@@ -99,15 +106,52 @@ app.post('/jobs', validateIdToken, (req, res) => {
       createdAt: admin.firestore.Timestamp.fromDate(new Date())
    };
 
-   admin
-      .firestore()
-      .collection('users').doc(req.body.uid).collection('jobs').doc(req.body.title).set(jobMap)
+   db.collection('users').doc(req.body.uid).collection('jobs').add(jobMap)
       .then(doc => {
          res.json({ message: `job ${doc.id} created successfully`})
       })
       .catch(err => {
          res.status(500).json({ error: err});
          console.log(err);
+      });
+});
+
+// Delete a job for a specific user ID and job title
+// TODO: create delete button in front end and add the title with req params
+// EX: <Link to={`/deletejob/${job.title}`}>{user.name}</Link>
+/*
+app.delete('/deletejob', validateIdToken, (req, res) => {
+   const jobRef = db.doc(`/users/${req.body.uid}/jobs/${req.body.title}`);
+   jobRef
+      .get()
+      .then((doc) => {
+         if (!doc.exists) {
+            return response.status(404).json({ error: 'Job not found' });
+         }
+         return document.delete();
+      })
+      .then(() => {
+         response.json({ message: 'Delete successful'});
+      })
+      .catch((err) => {
+         console.log(err);
+         return res.status(500).json({ error: err });
+      });
+});
+*/
+
+// Edit a job for a specific user.
+// TODO: May need to adjust req params or update params
+
+app.put('/editjob/:jobId', validateIdToken, (req, res) => {
+   let jobRef = db.collection('users').doc(req.body.uid).collection('jobs').doc(req.params.jobId);
+   jobRef.update({ title: req.body.newTitle, rate: req.body.newRate})
+      .then(() => {
+         res.json({ message: 'Updated successfully' });
+      })
+      .catch((err) => {
+         console.error(err);
+         return res.status(500).json({ error : err.code });
       });
 });
 
